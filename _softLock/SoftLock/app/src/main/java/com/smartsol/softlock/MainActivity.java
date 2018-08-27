ad77c52f5b23685f9a8e942723457281f0ab5fda
Prashanth Bachu <praskum@microsoft.com>
2018-08-28 01:25:38 +0530

    Soft Lock App

---
+++ _softLock/SoftLock/app/src/main/java/com/smartsol/softlock/MainActivity.java
@@ -0,0 +1,53 @@
+package com.smartsol.softlock;
+
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
+import android.widget.CompoundButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int ADMIN_INTENT = 1;
+    private DevicePolicyManager mDevicePolicyManager;
+    private ComponentName mComponentName;
+    SwitchCompat switchCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDevicePolicyManager = (DevicePolicyManager)getSystemService(
+                Context.DEVICE_POLICY_SERVICE);
+        mComponentName = new ComponentName(this, Admin.class);
+        switchCompat=(SwitchCompat)findViewById(R.id.switchButton);
+        if( mDevicePolicyManager != null && mDevicePolicyManager.isAdminActive(mComponentName)) {
+            switchCompat.setChecked(true);
+            mDevicePolicyManager.lockNow();
+            this.finish();
+        }
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if ((switchCompat).isChecked()) {
+                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName);
+                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Administrator description");
+                    startActivityForResult(intent, ADMIN_INTENT);
+                }
+                else {
+                    mDevicePolicyManager.removeActiveAdmin(mComponentName);
+                }
+            }
+        });
+    }
+}
