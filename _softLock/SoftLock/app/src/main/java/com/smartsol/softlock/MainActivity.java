8c2ebd8b6b8d0a0c00fb4d06957145a103afc4ab
prashanth kumar <prashaenator@gmail.com>
2018-12-25 19:50:35 +0530

    improving look&feel

--- _softLock/SoftLock/app/src/main/java/com/smartsol/softlock/MainActivity.java
+++ _softLock/SoftLock/app/src/main/java/com/smartsol/softlock/MainActivity.java
@@ -5,50 +5,79 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
-import android.util.Log;
-import android.widget.CompoundButton;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
     private static final int ADMIN_INTENT = 1;
     private DevicePolicyManager mDevicePolicyManager;
     private ComponentName mComponentName;
-    SwitchCompat switchCompat;
+    private Button enableButton;
+    private Button doneButton;
+    private LinearLayout permReqLayout, postPermGrantLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         mDevicePolicyManager = (DevicePolicyManager)getSystemService(
                 Context.DEVICE_POLICY_SERVICE);
         mComponentName = new ComponentName(this, Admin.class);
-        switchCompat=(SwitchCompat)findViewById(R.id.switchButton);
         if( mDevicePolicyManager != null && mDevicePolicyManager.isAdminActive(mComponentName)) {
-            switchCompat.setChecked(true);
+            //this.setTheme(android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_lock);
             mDevicePolicyManager.lockNow();
             this.finish();
         }
 
-        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.enableButton = (Button)findViewById(R.id.enableButton);
+        this.doneButton = (Button) findViewById(R.id.doneButton);
+        this.permReqLayout = (LinearLayout)findViewById(R.id.permRequestLayout);
+        this.postPermGrantLayout = (LinearLayout)findViewById(R.id.postPermGrantedLayout);
+        this.postPermGrantLayout.setVisibility(View.GONE);
+
+        this.enableButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if ((switchCompat).isChecked()) {
-                    Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                    intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName);
-                    intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Administrator description");
-                    startActivityForResult(intent, ADMIN_INTENT);
-                }
-                else {
-                    mDevicePolicyManager.removeActiveAdmin(mComponentName);
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName);
+               // intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Administrator description");
+                startActivityForResult(intent, ADMIN_INTENT);
             }
         });
+
+        this.doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitApp();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if( mDevicePolicyManager != null && mDevicePolicyManager.isAdminActive(mComponentName)) {
+            this.postPermGrantLayout.setVisibility(View.VISIBLE);
+            this.permReqLayout.setVisibility(View.GONE);
+        } else {
+            this.postPermGrantLayout.setVisibility(View.GONE);
+            this.permReqLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void exitApp() {
+        this.finish();
     }
 }
